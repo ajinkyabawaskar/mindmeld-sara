f75e9c1b3fdb3c53c268c4f21961a6bac0f01d3d
Manali Modi <manalimodi48@gmail.com>
2020-07-25 22:10:27 +0530

    wip

--- d_get_info.py
+++ d_get_info.py
@@ -3,7 +3,7 @@ from .root import app
 @app.handle(intent='get_india_info')
 def send_india_info(request, responder):
     query = request.text
-    answers = app.question_answerer.get(index='india', query_type='text', question=query, answer = query)
+    answers = app.question_answerer.build_search(index='india', query_type='text', question=query, answer = query)
     if answers:
         reply = ['Here is the top result:', answers[0]['question'], answers[0]['answer']]
         responder.reply('\n'.join(reply))

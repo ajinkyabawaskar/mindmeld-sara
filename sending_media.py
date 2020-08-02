431fe748a693170055bb3a9f6b1ec4eb89d5b1f2
xyashp <37106560+xyashp@users.noreply.github.com>
2020-08-02 16:39:42 +0530

    img url

--- sending_media.py
+++ sending_media.py
@@ -8,8 +8,8 @@ from_whatsapp_number='whatsapp:+14155238886'
 # replace this number with your personal WhatsApp Messaging number
 to_whatsapp_number='whatsapp:+919977216617'
 
-message = client.messages.create(body='Check out this owl!',
-                       media_url='https://demo.twilio.com/owl.png',
+message = client.messages.create(body='its your twitter profile right, Yash?',
+                       media_url='https://pbs.twimg.com/profile_images/1274045729170808833/2vT239Ac_400x400.jpg',
                        from_=from_whatsapp_number,
                        to=to_whatsapp_number)
 

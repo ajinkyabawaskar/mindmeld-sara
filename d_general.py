500e4c440a58f417b5a6d1f2445104c10ab72bf2
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2020-08-03 07:00:50 +0530

    WIp

--- d_general.py
+++ d_general.py
@@ -1,10 +1,10 @@
 from .root import app 
-import random
+import random, requests
 
 @app.handle(intent='greet')
 def welcome(request, responder):
-    responder.reply("Hi, I am Sara! Your virtual travel assistant. I can help you "
-    "plan your next visit to India 😍\n"
+    responder.reply("Hi, I am Sara! 💁‍♀️\n Your virtual travel assistant. I can help you "
+    "plan your next visit to India 🇮🇳\n"
     "You can tell me what places to go, or tell me what you want to experience, and I'll tell you about the places."
     " I can also check flight and hotel availability for you.")
 
@@ -25,14 +25,13 @@ def confirm_action(request, responder):
                 responder.slots['amount'] = homestays['amount']
                 responder.slots['contact'] = homestays['contact']
                 responder.slots['images'] = homestays['url']
-                responder.reply("Great! {name} has a {type} property that can accomodate"
+                responder.reply("Great! {name} has a {type} that can accomodate"
                                 " {people} people, and has {amenities}. It is available between"
                                 " {checkin} and {checkout}. Contact:{contact}  Images: {images}.\n Would you like to book for {amount}?")
                 responder.frame['expecting_homestay_confirmation'] = True
             except:
                 responder.frame['expecting_homestay_preference'] = True
                 responder.reply('Oops! Can you try again?')
-
         elif responder.frame.get('expecting_homestay_confirmation'):
             try:
                 responder.frame['expecting_homestay_confirmation'] = False
@@ -48,11 +47,36 @@ def confirm_action(request, responder):
                     responder.slots['password'] = response['password']
                 except:
                     responder.slots['password'] = 'afuy232vv'
-                responder.reply("Your homestay with {name} has been confirmed! Find your password protected invoice here:"
+                responder.reply("✅ Your homestay with {name} has been confirmed! Find your password protected invoice here:"
                 " https://myacademic.space/invoices/?homestay_id={homestay_id}B and password is {password}")
             except:
                 responder.frame['expecting_homestay_confirmation'] = True
                 responder.reply("Oops! Can you try again?")
+        elif responder.frame.get('flight_to_recommended_destination'):
+            responder.frame['flight_to_recommended_destination'] = False
+            responder.frame['flight_source_expected'] = True
+            responder.reply('Sure. Where will you be boarding from?')
+        elif responder.frame.get('hotel_to_recommended_destination'):
+            destination = responder.frame.get('recommended_destination')
+            try:
+                filter2 = responder.frame.get('filter')
+                responder.slots['filter'] = filter2
+            except:
+                responder.slots['filter'] = ' '
+
+            responder.slots['destination'] = destination
+            try:
+                hotels = app.question_answerer.get(index='locations', query_type='text', city=destination)
+                try:
+                    responder.frame['hotels'] = hotels
+                    responder.slots['hotels'] =", ".join(hotels[0]['hotels'])
+                    responder.reply("Here are some hotels at {destination}- {hotels}\n🏨 Where would you like to book?")
+                except:
+                    responder.reply("Sorry! There are no hotels at {destination} that I know of. 😪")
+            except:
+                responder.reply("I couldn't find any hotels at {destination}. 😪")
+        else:
+            responder.reply("Where do you want to visit?")
     except:
         responder.reply("Confirmed!")
 
@@ -64,7 +88,7 @@ def provide_help(request, responder):
     # Respond with examples demonstrating how the user can order food from different restaurants.
     # For simplicity, we have a fixed set of demonstrative queries here, but they could also be
     # randomly sampled from a pool of example queries each time.
-    replies = ["You can ask me to check flights for you.", "You can ask me to find hotels for you.", "You can tell me what your interests are and I can suggest you places to visit."]
+    replies = ["You can ask me to check flights for you. ✈️", "You can ask me to find hotels for you. 🏨", "You can tell me what your interests are and I can suggest you places to visit. ⛱"]
     responder.reply(replies)
 
 @app.handle(intent='exit')
@@ -73,10 +97,11 @@ def say_goodbye(request, responder):
     When the user ends a conversation, clear the dialogue frame and say goodbye.
     """
     # Clear the dialogue frame to start afresh for the next user request.
-    # responder.frame = {}
     
     if (responder.frame.get('expecting_homestay_preference') or responder.frame.get('expecting_homestay_confirmation')):
         destination = responder.frame.get('destination')
+        responder.frame['expecting_homestay_preference'] = False
+        responder.frame['expecting_homestay_confirmation'] = False
         try:
             filter2 = responder.frame.get('filter')
             responder.slots['filter'] = filter2
@@ -89,13 +114,18 @@ def say_goodbye(request, responder):
             try:
                 responder.frame['hotels'] = hotels
                 responder.slots['hotels'] =", ".join(hotels[0]['hotels'])
-                responder.reply("Here are some{filter}hotels at {destination}- {hotels}\n🏨 Where would you like to book?")
+                responder.reply("Cool. Alterntively, here are some{filter}hotels at {destination}- {hotels}\n🏨 Where would you like to book?")
             except:
                 responder.reply("Sorry! There are no hotels at {destination} that I know of. 😪")
         except:
             responder.reply("I couldn't find any hotels at {destination}. 😪")
+    elif responder.frame.get('flight_to_recommended_destination'):
+        responder.frame['flight_to_recommended_destination'] = False
+        responder.frame['hotel_to_recommended_destination'] = True
+        responder.reply('No probs. Would you like to find hotels there?')
     else:
         # Respond with a random selection from one of the canned "goodbye" responses.
+        responder.frame = {}
         responder.reply(['Bye! 👋', 'Goodbye! 👋', 'Have a nice day. 👋', 'See you later. 👋'])
     
 @app.handle(intent='unsupported')
@@ -106,3 +136,4 @@ def say_unsupported(request, responder):
     """
     replies = ["Sorry, I couldn't understand it. 😢"]
     responder.reply(replies)
+
